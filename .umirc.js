86a6b900adf2abe3acf84622ca69d01f2f33e8d1
herui <675021508@qq.com>
2020-05-03 13:23:09 +0800

    umi-react

---
+++ .umirc.js
@@ -0,0 +1,40 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  targets: {
+    ie: 11,
+  },
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' }
+      ]
+    }
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'hiatmp-umi-app',
+      dll: true,
+      locale: {
+        enable: true,
+        default: 'en-US',
+      },
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
